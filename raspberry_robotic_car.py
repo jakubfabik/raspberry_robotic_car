9f988a22a81efcd53a71b1c041cec5ad337de4f9
eof <fabikjakub@outlook.com>
2019-03-22 17:50:36 +0100

    added short circuit prevention

--- raspberry_robotic_car.py
+++ raspberry_robotic_car.py
@@ -14,47 +14,47 @@ class robot:
     def left(self, switch=True, status=None):
         if status != None:
             switch = False
-            if status == "on":
+            if status == "on" and self.RIGHT.is_active == True :
                 self.LEFT.off()
             if status == "off":
                 self.LEFT.on()
         if switch:
-            if self.LEFT.is_active:
+            if self.LEFT.is_active and self.RIGHT.is_active == True :
                 self.LEFT.off()
             else: self.LEFT.on()
     
     def right(self, switch=True, status=None):
         if status != None:
             switch = False
-            if status == "on":
+            if status == "on" and self.LEFT.is_active == True :
                 self.RIGHT.off()
             if status == "off":
                 self.RIGHT.on()
         if switch:
-            if self.RIGHT.is_active:
+            if self.RIGHT.is_active and self.LEFT.is_active == True :
                 self.RIGHT.off()
             else: self.RIGHT.on()
 
     def forward(self, switch=True, status=None):
         if status != None:
             switch = False
-            if status == "on":
+            if status == "on" and self.BACKWARD.is_active == True :
                 self.FORWARD.off()
             if status == "off":
                 self.FORWARD.on()
         if switch:
-            if self.FORWARD.is_active:
+            if self.FORWARD.is_active and self.BACKWARD.is_active == True :
                 self.FORWARD.off()
             else: self.FORWARD.on()
 
     def backward(self, switch=True, status=None):
         if status != None:
             switch = False
-            if status == "on":
+            if status == "on" and self.FORWARD.is_active == True :
                 self.BACKWARD.off()
             if status == "off":
                 self.BACKWARD.on()
         if switch:
-            if self.BACKWARD.is_active:
+            if self.BACKWARD.is_active and self.FORWARD.is_active == True :
                 self.BACKWARD.off()
             else: self.BACKWARD.on()
