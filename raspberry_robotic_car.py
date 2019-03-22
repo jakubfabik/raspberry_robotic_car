2b054c92ac1fc3e2d9c78795a8bc069467ea7492
eof <fabikjakub@outlook.com>
2019-03-22 14:53:45 +0100

    Change of app idea, begining from 0

--- raspberry_robotic_car.py
+++ raspberry_robotic_car.py
@@ -1,28 +1,60 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED as pin
 import time
-
-
-
-GPIO.output(4, GPIO.HIGH)                                                           #pin on voltage
-GPIO.output(4, GPIO.LOW)                                                            #grounded pin
 
 class robot:
     def __init__(self):
-        pin = {"left":4, "ryght":17, "forward":27, "backward":22}                   #pins in the use
-        for key in pin:
-            GPIO.setup(pin.pop(key), GPIO.OUT)                                      #initializing pin
-            GPIO.output(pin.pop(key), GPIO.LOW)                                     #switching pins off
-            print(f"initializing pin {key}")
+        self.LEFT = pin(19)
+        self.RIGHT = pin(16)
+        self.FORWARD = pin(26)
+        self.BACKWARD = pin(20)
+        functions = {self.LEFT, self.RIGHT, self.FORWARD, self.BACKWARD}
+        for function in functions:
+            function.on()
 
-    def on(self, part):
-        return GPIO.output(self.pin[part], GPIO.HIGH)                               #turn on name of pin
+    def left(self, switch=True, status=None):
+        if status != None:
+            switch = False
+            if status == "on":
+                self.LEFT.off()
+            if status == "off":
+                self.LEFT.on()
+        if switch:
+            if self.LEFT.is_active:
+                self.LEFT.off()
+            else: self.LEFT.on()
+    
+    def right(self, switch=True, status=None):
+        if status != None:
+            switch = False
+            if status == "on":
+                self.RIGHT.off()
+            if status == "off":
+                self.RIGHT.on()
+        if switch:
+            if self.RIGHT.is_active:
+                self.RIGHT.off()
+            else: self.RIGHT.on()
 
-    def off(self, part):
-        return GPIO.output(self.pin[part], GPIO.LOW)                                #turn off name of pin
+    def forward(self, switch=True, status=None):
+        if status != None:
+            switch = False
+            if status == "on":
+                self.FORWARD.off()
+            if status == "off":
+                self.FORWARD.on()
+        if switch:
+            if self.FORWARD.is_active:
+                self.FORWARD.off()
+            else: self.FORWARD.on()
 
-    def switch(self, part):                                                         #switch logic value of pin
-        if GPIO.input(self.pin[part]):
-            return GPIO.output(self.pin[part], GPIO.LOW)
-        else:
-            return GPIO.output(self.pin[part], GPIO.HIGH)
-
+    def backward(self, switch=True, status=None):
+        if status != None:
+            switch = False
+            if status == "on":
+                self.BACKWARD.off()
+            if status == "off":
+                self.BACKWARD.on()
+        if switch:
+            if self.BACKWARD.is_active:
+                self.BACKWARD.off()
+            else: self.BACKWARD.on()
