7bb06c8c0841987bd787dc4b01b1f4ccf417c136
eof <fabikjakub@outlook.com>
2019-03-22 11:04:41 +0100

    Added manipulation with pins class robot

--- raspberry_robotic_car.py
+++ raspberry_robotic_car.py
@@ -1,13 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
 
-#4,17,27,22 
-GPIO.setup(4, GPIO.OUT)     #initialising pin
-GPIO.setup(17, GPIO.OUT)    #initialising pin
-GPIO.setup(27, GPIO.OUT)    #initialising pin
-GPIO.setup(22, GPIO.OUT)    #initialising pin
 
-GPIO.output(4, GPIO.HIGH)   #pin on voltage
-GPIO.output(4, GPIO.LOW)    #grounded pin
+GPIO.output(4, GPIO.HIGH)                                                           #pin on voltage
+GPIO.output(4, GPIO.LOW)                                                            #grounded pin
+
+class robot:
+    def __init__(self):
+        pin = {"left":4, "ryght":17, "forward":27, "backward":22}                   #pins in the use
+        for key in pin:
+            GPIO.setup(pin.pop(key), GPIO.OUT)                                      #initializing pin
+            GPIO.output(pin.pop(key), GPIO.LOW)                                     #switching pins off
+            print(f"initializing pin {key}")
+
+    def on(self, part):
+        return GPIO.output(self.pin[part], GPIO.HIGH)                               #turn on name of pin
+
+    def off(self, part):
+        return GPIO.output(self.pin[part], GPIO.LOW)                                #turn off name of pin
+
+    def switch(self, part):                                                         #switch logic value of pin
+        if GPIO.input(self.pin[part]):
+            return GPIO.output(self.pin[part], GPIO.LOW)
+        else:
+            return GPIO.output(self.pin[part], GPIO.HIGH)
+
