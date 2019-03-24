50afdcd46d75c12fe663f70dab9b69da0a3ec2a5
eof <fabikjakub@outlook.com>
2019-03-24 15:26:09 +0100

    added move by step function

--- raspberry_robotic_car.py
+++ raspberry_robotic_car.py
@@ -1,5 +1,6 @@
 from gpiozero import LED as pin
 import time
+import getch
 
 class robot:
     def __init__(self):
@@ -8,10 +9,10 @@ class robot:
         self.FORWARD = pin(26)
         self.BACKWARD = pin(20)
         functions = {self.LEFT, self.RIGHT, self.FORWARD, self.BACKWARD}
-        for function in functions:
-            function.on()
+        for function in functions:                                          # by defaut are pins on input high == on 
+            function.on()                                                   # to switch on means switch off
 
-    def left(self, switch=True, status=None):
+    def left(self, switch=True, status=None):                               # left switch and check right can not be on
         if status != None:
             switch = False
             if status == "on" and self.RIGHT.is_active == True :
@@ -23,7 +24,7 @@ class robot:
                 self.LEFT.off()
             else: self.LEFT.on()
     
-    def right(self, switch=True, status=None):
+    def right(self, switch=True, status=None):                              # right switch and check left can not be on
         if status != None:
             switch = False
             if status == "on" and self.LEFT.is_active == True :
@@ -35,7 +36,7 @@ class robot:
                 self.RIGHT.off()
             else: self.RIGHT.on()
 
-    def forward(self, switch=True, status=None):
+    def forward(self, switch=True, status=None):                            # forward switch and check backward can not be on
         if status != None:
             switch = False
             if status == "on" and self.BACKWARD.is_active == True :
@@ -47,7 +48,7 @@ class robot:
                 self.FORWARD.off()
             else: self.FORWARD.on()
 
-    def backward(self, switch=True, status=None):
+    def backward(self, switch=True, status=None):                           # backward switch and chack forward can not be on
         if status != None:
             switch = False
             if status == "on" and self.FORWARD.is_active == True :
@@ -58,3 +59,32 @@ class robot:
             if self.BACKWARD.is_active and self.FORWARD.is_active == True :
                 self.BACKWARD.off()
             else: self.BACKWARD.on()
+
+def navigate(robot):                                        #moves by steps
+    def key(robot, cont):
+        if cont == "d":
+            robot.right()
+            time.sleep(1)
+            robot.right()
+    
+        if cont == "a":
+            robot.left()
+            time.sleep(1)
+            robot.left()
+
+        if cont == "w":
+            robot.forward()
+            time.sleep(1)
+            robot.forward()
+        
+        if cont == "s":
+            robot.backward()
+            time.sleep(1)
+            robot.backward()
+
+    while True:
+        print("to stop input function press 'e'\n")
+        cont = getch.getch()
+        key(robot, cont)
+        if cont == "e":
+            break
